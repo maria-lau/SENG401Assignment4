4156a9183551ac19605cd4218b4719acf65a08e0
Maria <m.marialau@gmail.com>
2018-02-21 02:21:36 -0700

    Initial Project Creation

---
+++ WebApplication2/Controllers/ReviewController.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class ReviewController : ApiController
+    {
+        // GET: api/Review
+        public IEnumerable<ReviewInfo> Get()
+        {
+            //should be using POST instead of GET to save reviews.
+            var reviewInfoList = new List<ReviewInfo>();
+            for(int i = 0; i < 10; i++)
+            {
+                var reviewInfo = new ReviewInfo
+                {
+                    companyName = $"companyName {i}",
+                    username = $"username {i}",
+                    review = $"review {i}",
+                    stars = i,
+                    timestamp = (Int32)DateTime.UtcNow.Subtract(new DateTime(1970, 1, 1)).TotalSeconds
+                };
+                reviewInfoList.Add(reviewInfo);
+            }
+            return reviewInfoList;
+        }
+
+        // GET: api/Review/5
+        public ReviewInfo Get(int id)
+        {
+            return new ReviewInfo
+            {
+                companyName = $"companyName {id}",
+                username = $"username {id}",
+                review = $"review {id}",
+                stars = id,
+                timestamp = (Int32)DateTime.UtcNow.Subtract(new DateTime(1970, 1, 1)).TotalSeconds
+            };
+        }
+
+        // POST: api/Review
+        public void Post([FromBody]string value)
+        {
+            var reviewInfoList = new List<ReviewInfo>();
+            for (int i = 0; i < 10; i++)
+            {
+                var reviewInfo = new ReviewInfo
+                {
+                    companyName = $"companyName {i}",
+                    username = $"username {i}",
+                    review = $"review {i}",
+                    stars = i,
+                    timestamp = (Int32)DateTime.UtcNow.Subtract(new DateTime(1970, 1, 1)).TotalSeconds
+                };
+                reviewInfoList.Add(reviewInfo);
+            }
+        }
+
+        // PUT: api/Review/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Review/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ WebApplication2/Models/ReviewInfo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Runtime.Serialization;
+
+namespace WebApplication2.Models
+{
+    [DataContract]
+    public class ReviewInfo
+    {
+        [DataMember(Name = "companyName")]
+        public string companyName { get; set; }
+
+        [DataMember(Name = "username")]
+        public string username { get; set; }
+
+        [DataMember(Name = "review")]
+        public string review { get; set; }
+
+        [DataMember(Name = "stars")]
+        public double stars { get; set; }
+
+        [DataMember(Name = "timestamp")]
+        public Int32 timestamp { get; set; }
+    }
+}
